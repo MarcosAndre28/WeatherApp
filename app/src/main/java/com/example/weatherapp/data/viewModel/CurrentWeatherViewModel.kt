bb05b1fcca701751ef829fd0751cffc59b295a93
MarcosAndre28 <marcosandre1728@gmail.com>
2023-02-18 23:47:14 -0300

    implementation and navigation

---
+++ app/src/main/java/com/example/weatherapp/data/viewModel/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.data.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+}
